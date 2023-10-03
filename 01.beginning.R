7b532a317e4cee35b2fc6b53c81376ce7e2d2cdb
anitacasanovademarco <146854643+anitacasanovademarco@users.noreply.github.com>
2023-10-03 17:48:12 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -8,3 +8,18 @@ duccio <- 5 + 3
 zima * duccio
 
 final <- zima * duccio
+
+
+# Array
+sophi <- c(10,20,30,50,70) # microplastics (arguments) 
+# functions always have parenthesis 
+paula <- c(100, 500, 600, 1000, 2000) #people
+plot(paula, sophi, xlab="number of people", ylab="microplastics")
+# we can also do like that
+people <- paula
+microplastics <- sophi
+plot (people, microplastics, pch=19, cex=2, col="blue")
+# http://www.sthda.com/english/wiki/r-plot-pch-symbols-the-different-point-shapes-available-in-r
+
+
+
