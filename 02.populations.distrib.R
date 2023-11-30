2903e9e92fd4351c77014300a414b44a62588295
anitacasanovademarco <146854643+anitacasanovademarco@users.noreply.github.com>
2023-11-30 18:32:34 +0100

    Update 02.populations.distrib.R

--- 02.populations.distrib.R
+++ 02.populations.distrib.R
@@ -1,21 +1,22 @@
 ########## SPECIES DISTRIBUTION
-
+# why populations disperse in a certain manner over the landscape?
+#INSTALL PACKAGES (with quotes cause they're outside R)
 install.packages("sdm")
-install.packages("rgdal", dependencies=T) #now rgdal is inside terra package
+install.packages("rgdal", dependencies=T) #Warning Message: rgdal will be retired cause it'll be included in terra package
 
 #OPEN THE DOWNLOADED PACKAGES (no more quotes cause now they're inside R)
-library(sdm) # sdm = species distribution modelling
-library(terra) # used for spatial functions (predictors)
-library(rgdal) # gdal = translator library for raster and vector geospatial data formats 
+library(sdm) # Species Distribution Modelling
+library(terra) # spatial predictors, that are environmental variables
+library(rgdal) # translator library for raster and vector geospatial data formats 
 
-#SELECT THE FILE INSIDE A PACKAGE
-file <- system.file("external/species.shp", package="sdm") #inside sdm there's external, which contains the file "species.shp"
+#SELECT THE FILE INSIDE THE PACKAGE sdm
+file <- system.file("external/species.shp", package="sdm") 
+# sdm have vector files and images
 
-# shp = files usually called VECTOR FILES, that are series of coordinates
+#########let's import a vector, which is a coordinate system (function: vect)
+species <- vect(file) 
 
-species <- vect(file) #let's import a vector of  this file: if we run it we get a COORDINATE SYSTEM
-
-#to see Occurrence just link the vector to Occurrence, like this
+#to see Occurrence just link the vector to Occurrence with the $
 species$Occurrence # PRESENCE-ABSENCE DATA
 #0 is an uncertain data because maybe she's hidden, so let's take it in this case like a REAL 0
 
@@ -47,17 +48,16 @@ plot(abs, col="light blue")
 plot(pres, col="dark blue")
 points(abse, col="light blue")
 
-#PREDICTORS: environmental variables
-
+######let's import an image (function: rast)
 #Elevation predictor
-elev <- system.file("external/elevation.asc", package="sdm") #asc is an extention and it's a type of file, like png 
-elevmap <-rast(elev) #to import the file, like vec before: now we get an image because we wrote rast instead of  like an image, we write rast, and not vec anymore, like before
+elev <- system.file("external/elevation.asc", package="sdm") #asc is an extention of sdm and it's a type of file, like png 
+elevmap <-rast(elev) 
 plot(elevmap)
 points(pres,cex=0.5) #we see that the specie, which is in this case, Rana temporaria, is avoiding a valley. More, it's not choosing very low and very high elevations
 
 #Temperature predictor
-temp <- system.file("external/temperature.asc", package="sdm") #asc is an extention and it's a type of file, like png 
-tempmap <-rast(temp) #to import the file, like vec before: now we get an image because we wrote rast instead of  like an image, we write rast, and not vec anymore, like before
+temp <- system.file("external/temperature.asc", package="sdm") 
+tempmap <-rast(temp) 
 plot(tempmap)
 points(pres,cex=0.5)
 
@@ -67,7 +67,7 @@ vegmap <-rast(veg)
 plot(vegmap)
 points(pres,cex=0.5)
 
-#Precipitation
+#Precipitation predictor
 prec <- system.file("external/precipitation.asc", package="sdm") 
 precmap <-rast(prec) 
 plot(precmap)
@@ -76,7 +76,7 @@ points(pres,cex=0.5)
 #Final multiframe
 par(mfrow=c(2,2))
 
- #elevation
+#elevation
 plot(elevmap)
 points(pres,cex=0.5)
 
@@ -91,6 +91,8 @@ points(pres,cex=0.5)
 #precipitation
 plot(precmap)
 points(pres,cex=0.5)
+
+
 
 
 
