caf588a02f10613f2b5fe448559e3da2f4cc19ca
anitacasanovademarco <146854643+anitacasanovademarco@users.noreply.github.com>
2023-11-30 16:03:16 +0100

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -1,24 +1,35 @@
 # Here I can write anything I want
+
 # R as a caluclator
 3+4
+
 # Assign to an object
 zima <- 2 + 3
 
 duccio <- 5 + 3
-zima * duccio
 
+# Calculations
+zima * duccio
 final <- zima * duccio
 
 
-# Array
-sophi <- c(10,20,30,50,70) # microplastics (arguments) 
-# functions always have parenthesis 
+##### Array
+# they're functions
+# fuctions always have parenthesis and inside them there're the arguments
+
+# Examples
+sophi <- c(10,20,30,50,70) # microplastics
 paula <- c(100, 500, 600, 1000, 2000) #people
+
+# we can plot them together
 plot(paula, sophi, xlab="number of people", ylab="microplastics")
-# we can also do like that
+
+# we can also previously assign paula and sophi to some objects
 people <- paula
 microplastics <- sophi
-plot (people, microplastics, pch=19, cex=2, col="blue")
+plot (people, microplastics, pch=19, cex=2, col="blue") # pch gives the shape of the symbols in R, cex represents the size, col for the color
+
+# we can find the symbols in this site:
 # http://www.sthda.com/english/wiki/r-plot-pch-symbols-the-different-point-shapes-available-in-r
 
 
