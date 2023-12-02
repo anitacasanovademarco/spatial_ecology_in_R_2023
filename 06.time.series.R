839832ac73cf6066a4b08fd154a21880296fea59
anitacasanovademarco <146854643+anitacasanovademarco@users.noreply.github.com>
2023-12-02 12:07:44 +0100

    Update 06.time.series.R

--- 06.time.series.R
+++ 06.time.series.R
@@ -3,9 +3,11 @@ library(terra)
 library(imageRy)
 im.list()
 
-# COVID effects on the environment: nitrogen concentration from january to march 2020
-EN01<-im.import("EN_01.png") #European Nitrogen
-EN13<-im.import("EN_13.png")
+#### CHANGES IN NITROGEN CONCENTRATION due to COVID, from January to March 2020
+EN01<-im.import("EN_01.png") #European Nitrogen in January
+EN13<-im.import("EN_13.png") #European Nitrogen in March
+EN01
+EN13
 
 par(mfrow=c(2,1))
 im.plotRGB.auto(EN01)
@@ -16,35 +18,35 @@ dev.off()
 diff=EN01[[1]]-EN13[[1]]
 plot(diff)
 
-col <- colorRampPalette(c('red','orange','yellow'))(100)  
-#Reminder: blue and red or green and red aren't good for dalthonic people
-cl <- colorRampPalette(c('blue','white','red'))(100) 
-plot(diff,col=cl) 
-#blue = is higher in march, red = is higher in january
-#we see that in many cities people stopped to use cars
-#so we have a huge decrease in march
+#Reminder: blue and red or green and red aren't good for daltonic people
+#So, let's change the colors
 
-# CHANGES IN TEMPERATURE of Greenland ice sheet
+cl <- colorRampPalette(c('red','orange','yellow'))(100)  
+plot(diff,col=cl) 
+#red is higher in march, yellow is higher in january
+#so we have a huge decrease in march (there's more yellow than red)
+#we see that in many cities people stopped to use cars
+
+##### CHANGES IN TEMPERATURE of Greenland ice sheet
 
 dev.off()
 im.list()
 G2000<-im.import("greenland.2000.tif") #16 bits image
 plot(G2000,col=cl) #temperature on the surface of the land
-#blue=almost perennial ice cap
+#red=almost perennial ice cap
 
 G2005<-im.import("greenland.2005.tif")
 G2010<-im.import("greenland.2010.tif")
 G2015<-im.import("greenland.2015.tif")
 
+par(mfrow=c(2,2))
+plot(G2000,col=cl)
 plot(G2005,col=cl)
 plot(G2010,col=cl)
 plot(G2015,col=cl)
 
 #to make it more effective
 cl1 <- colorRampPalette(c('black', 'blue','white','red'))(100) 
-plot(G2015,col=cl1)
-
-dev.off()
 par(mfrow=c(2,2))
 plot(G2000,col=cl1)
 plot(G2005,col=cl1)
@@ -54,30 +56,31 @@ plot(G2015,col=cl1)
 #it's the same as
 
 dev.off()
-stackG<-c(G2000,G2005,G2010,G2015) #around 2000 there was the worst period
-plot(stackG, col=cl1)
+stackG4<-c(G2000,G2005,G2010,G2015) #around 2005 there was the worst period
+plot(stackG4, col=cl1)
+#we see that the surface temperature in Greenland have increased and then decreased back, while that in the Nunavut has gradually increased
+#also Island's temperature has decreased a little bit
 
 #or only the first and the last
-
-par(mfrow=c(1,2))
-plot(G2000,col=cl)
-plot(G2015,col=cl)
+dev.off()
+stackG2<-c(G2000,G2015)
+plot(stackG2, col=cl1)
 
 #Make the difference between the first and the last element
-diffg<-stackG[[1]]-stackG[[4]]
+diffg<-stackG4[[1]]-stackG4[[4]]
 #it's the same as
 difg<-G2000-G2015
-plot(diffg, col=cl) 
-#blue=temperature was lower in the past
-#so there's a lost of low temperatures (so ice) and increase of temperature
+plot(diffg, col=cl1) 
+#blue=temperature was lower in the past 
+#so there's a lot of increase in temperatures, therefore so decrease in ice sheet
 #red= temperature was higher in the past
 
 #Make a RGB plot using different years
 dev.off()
-im.plotRGB(stackG, r=1,g=2,b=3)
-#blue = temperature was lower in the past (central part)
-#green = temperture similar to the past
-#red = temperature was higher in the past (external part)
+im.plotRGB(stackG4, r=1,g=2,b=3)
+#red = temperature was lower in the past (external part)
+#green = temperature similar to the past
+#blue = temperature was higher in the past (central part)
 
 
 
