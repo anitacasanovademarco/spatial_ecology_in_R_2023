cc7813d731f1e25d472b2af5249df60c9b60aad3
anitacasanovademarco <146854643+anitacasanovademarco@users.noreply.github.com>
2024-01-09 16:38:34 +0100

    Update 03.communities.overlap.R

--- 03.communities.overlap.R
+++ 03.communities.overlap.R
@@ -2,24 +2,24 @@
 install.packages("overlap")
 library(overlap)
 
-data(kerinci) #data from Kerinci-Seblat National Park in Sumatra, Indonesia
+data(kerinci) # data from Kerinci-Seblat National Park in Sumatra, Indonesia
 kerinci
 head(kerinci)
 summary(kerinci)
 
-#Selecting the first species
-tiger <- kerinci[kerinci$Sps=="tiger",] #Sps=species
+# Selecting the first species
+tiger <- kerinci[kerinci$Sps=="tiger",] # Sps=species
 
-#Selecting the tigers time
+# Selecting the tigers time
 timetig <- kerinci$Time*2*pi 
-#we multiply the the linear time to 2 pi greek to have a time in radiants
+# we multiply the the linear time to 2 pi greek to have a time in radiants
 
 densityPlot(timetig, rug=TRUE)
 
-#Selecting the macaque
-macaque <- kerinci[kerinci$Sps=="macaque",] #Sps=species
+# Selecting the macaque
+macaque <- kerinci[kerinci$Sps=="macaque",] # Sps=species
 
-#Selecting the macaque time
+# Selecting the macaque time
 timemac <- macaque$Time*2*pi 
 
 densityPlot(timemac, rug=TRUE)
